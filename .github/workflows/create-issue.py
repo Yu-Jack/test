28dba125264beb1c66cfa374116675273c7e0a18
Jack Yu <jack.yu@suse.com>
2024-12-30 14:23:00 +0800

    tset
    Signed-off-by: Jack Yu <jack.yu@suse.com>

--- .github/workflows/create-issue.py
+++ .github/workflows/create-issue.py
@@ -1,7 +1,18 @@
 import sys
+import json
 
 if __name__ == "__main__":
     request = sys.argv[1]
     sec = sys.argv[2]
     print(request)
     print(sec)
+    
+    try:
+        # 解析 JSON 字符串
+        data = json.loads(request)
+        print("Parsed JSON:", data)
+    except json.JSONDecodeError as e:
+        print("Failed to parse JSON:", e)
+        
+        
+    print(data["issue"]["title"])
